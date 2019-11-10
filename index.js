528cadd07d23a31a641bf087ecf97fbf38452248
Joel Perez <joelness03@gmail.com>
2019-11-10 00:49:41 -0700

    basic queries with variables

--- index.js
+++ index.js
@@ -2,13 +2,31 @@ var express = require("express");
 var graphqlHTTP = require("express-graphql");
 var { buildSchema } = require("graphql");
 
+// Construct a schema, using GraphQL schema language
 var schema = buildSchema(`
   type Query {
-    hello: String
+    quoteOfTheDay: String
+    random: Float!
+    rollTheDice(numDice: Int!, numSides: Int): [Int]
   }
 `);
 
-var root = { hello: () => "Hello world!" };
+// The root provides a resolver function for each API endpoint
+var root = {
+  quoteOfTheDay: () => {
+    return Math.random() < 0.5 ? "Take it easy" : " Hope lies within";
+  },
+  random: () => {
+    return Math.random();
+  },
+  rollTheDice: ({ numDice, numSides }) => {
+    let output = [];
+    for (let i = 0; i < numDice; i++) {
+      output.push(1 + Math.floor(Math.random() * (numSides || 6)));
+    }
+    return output;
+  }
+};
 
 var app = express();
 app.use(
@@ -19,4 +37,5 @@ app.use(
     graphiql: true
   })
 );
-app.listen(4000, () => console.log("Now browse to localhost:4000/graphql"));
+app.listen(4000);
+console.log("Now browse to localhost:4000/graphql");
